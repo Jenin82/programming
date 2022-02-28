af03351595b79168696eb43ef719577479406995
Parzival <jenin8282@gmail.com>
2022-02-28 21:25:38 +0530

    v.1.0

--- java_program/primeOrNot/Test.java
+++ java_program/primeOrNot/Test.java
@@ -3,12 +3,12 @@ public class Test {
   public static void main(String[] args) {
   System.out.println("Enter number");
   Scanner in = new Scanner(System.in);
-  int x=in.nextInt();
-    int flag = 0;
-    for (int i = 2; i <= x/ 2; ++i) {
-      if (x % i == 0) {
-        flag = 1;
-        break;
+    int x=in.nextInt();
+      int flag = 0;
+      for (int i = 2; i <= x/ 2; ++i) {
+        if (x % i == 0) {
+          flag = 1;
+          break;
       }
     }
     if (flag==0)
