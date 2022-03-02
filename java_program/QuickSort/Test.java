f05b06f54efdd4ad40188a13fbb536ac3eef05a8
Parzival <jenin8282@gmail.com>
2022-03-03 05:07:31 +0530

    v.1.0

---
+++ java_program/QuickSort/Test.java
@@ -0,0 +1,60 @@
+public class Test {
+
+	String names[];
+	int length;
+
+	public static void main(String[] args) {
+			Test sorter = new Test();
+			String words[] = {"zz", "aa", "cc", "hh", "bb", "ee", "ll"}; // the strings need to be sorted are put inside this array
+			sorter.sort(words);
+
+			for (String i : words) {
+					System.out.print(i);
+					System.out.print(" ");
+			}
+	}
+
+	void sort(String array[]) {
+			if (array == null || array.length == 0) {
+					return;
+			}
+			this.names = array;
+			this.length = array.length;
+			quickSort(0, length - 1);
+	}
+
+	void quickSort(int lowerIndex, int higherIndex) {
+			int i = lowerIndex;
+			int j = higherIndex;
+			String pivot = this.names[lowerIndex + (higherIndex - lowerIndex) / 2];
+
+			while (i <= j) {
+					while (this.names[i].compareToIgnoreCase(pivot) < 0) {
+							i++;
+					}
+
+					while (this.names[j].compareToIgnoreCase(pivot) > 0) {
+							j--;
+					}
+
+					if (i <= j) {
+							exchangeNames(i, j);
+							i++;
+							j--;
+					}
+			}
+			//call quickSort recursively
+			if (lowerIndex < j) {
+					quickSort(lowerIndex, j);
+			}
+			if (i < higherIndex) {
+					quickSort(i, higherIndex);
+			}
+	}
+
+	void exchangeNames(int i, int j) {
+			String temp = this.names[i];
+			this.names[i] = this.names[j];
+			this.names[j] = temp;
+	}
+}
--- java_program/Testing/Test.java
+++ java_program/Testing/Test.java
@@ -1,3 +1,41 @@
-public class Test {
+import javax.swing.*;
+import java.awt.*;
+
+class TrafficLight extends JPanel {
 	
+	private JRadioButton r1;
+	private JRadioButton r2;
+	private JRadioButton r3;
+	
+	public TrafficLight(){
+		setBounds(0,0,400,300);
+		r1 = new JRadioButton("Red");
+		r2 = new JRadioButton("Yellow");
+		r3 = new JRadioButton("Green");
+		r1.setSelected(true);
+		ButtonGroup gp = new ButtonGroup();
+		gp.add(r1);
+		gp.add(r2);
+		gp.add(r3);
+		add(r1);
+		add(r2);
+		add(r3);
+	}
+
+	public void paintComponent(Graphics g){
+		g.drawOval(120, 50, 50, 50);
+		g.drawOval(120, 110, 50, 50);
+		g.drawOval(120, 170, 50, 50);
+	}
+}
+
+public class Test{
+	public static void main(String[] args) {
+		JFrame j = new JFrame();
+		j.setLayout(null);
+		j.setBounds(100, 100, 400, 300);
+		j.setVisible(true);
+		TrafficLight t = new TrafficLight();
+		j.add(t);
+	}
 }
