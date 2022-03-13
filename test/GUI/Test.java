a5edd13890ef6000c2b37da8d38cc320d8851cf0
Jenin82 <jenin8282@gmail.com>
2022-03-13 18:56:32 +0530

    v.1.0

---
+++ test/GUI/Test.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+public class Test {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Enter your name");
+		JOptionPane.showMessageDialog(null, "Hello "+name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "You are "+age+" years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null, "You are "+height+" cm tall");
+
+	}
+}
