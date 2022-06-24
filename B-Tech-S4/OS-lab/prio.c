e3107c2bb74561269da04ea09eda009a1a06d7d9
Jenin82 <jenin8282@gmail.com>
2022-06-24 18:35:08 +0530

    new branch

--- B-Tech-S4/OS-lab/prio.c
+++ B-Tech-S4/OS-lab/prio.c
@@ -5,35 +5,23 @@ int main()
     int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
     printf("Enter Total Number of Process:");
     scanf("%d",&n);
- 
     printf("\nEnter Burst Time and Priority\n");
-    for(i=0;i<n;i++)
-    {
-        printf("\nP[%d] \n",i+1);
-        //printf("Burst Time:");
-        scanf("%d",&bt[i]);
-        //printf("Priority:");
-        scanf("%d",&pr[i]);
+    for(i=0;i<n;i++) {
+        printf("\nP[%d] :\n",i+1);
+        scanf("%d%d",&bt[i],&pr[i]);
         p[i]=i+1;         
     }
- 
-    for(i=0;i<n;i++)
-    {
+    for(i=0;i<n;i++) {
         pos=i;
         for(j=i+1;j<n;j++)
-        {
             if(pr[j]<pr[pos])
                 pos=j;
-        }
- 
         temp=pr[i];
         pr[i]=pr[pos];
         pr[pos]=temp;
- 
         temp=bt[i];
         bt[i]=bt[pos];
         bt[pos]=temp;
- 
         temp=p[i];
         p[i]=p[pos];
         p[pos]=temp;
