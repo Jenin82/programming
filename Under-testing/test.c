00d48118a3310abead494d5559ce93c39d021cad
Jenin82 <jenin8282@gmail.com>
2022-06-24 20:13:43 +0530

    edited looks

--- Under-testing/test.c
+++ Under-testing/test.c
@@ -1,4 +1,24 @@
+Enter Total Number of Process:4
 
+Enter Burst Time and Priority
 
+P[1] :
+2 6
 
+P[2] :
+8 0
 
+P[3] :
+5 2
+
+P[4] :
+12 8
+
+Process     Burst Time          Waiting Time    Turnaround Time
+P[2]              8                 0                   8
+P[3]              5                 8                   13
+P[1]              2                 13                  15
+P[4]              12                15                  27
+
+Average Waiting Time=9
+Average Turnaround Time=15
