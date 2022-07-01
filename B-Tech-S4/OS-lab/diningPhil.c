d883b2cc20bf0e07b4407a297b875ea5cf6bb415
Jenin82 <jenin8282@gmail.com>
2022-07-01 20:36:20 +0530

    completed dining philosophers program

---
+++ B-Tech-S4/OS-lab/diningPhil.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int n,i,j;
+void main(){
+	printf("Enter the number of philosophers :");
+	scanf("%d",&n);
+	printf("One person eats at a time\n");
+	for(i=1;i<n+1;i++) {
+		printf("\nDining :%d\nPhilosopher %d is Eating\n",i,i);
+		for(j=i+1;j<n+1;j++) 
+			printf("Philosopher %d is Waiting\n",j);
+		int k = i;
+		for(i=1;i<k;i++) 
+			printf("Philosopher %d is Thinking\n",i);
+	}
+	printf("\nAll philosophers are now Thinking\n");
+}
--- Under-testing/test.c
+++ Under-testing/test.c
@@ -1,4 +1,39 @@
+Enter the number of philosophers :5
+One person eats at a time
 
+Dining :1
+Philosopher 1 is Eating
+Philosopher 2 is Waiting
+Philosopher 3 is Waiting
+Philosopher 4 is Waiting
+Philosopher 5 is Waiting
 
+Dining :2
+Philosopher 2 is Eating
+Philosopher 3 is Waiting
+Philosopher 4 is Waiting
+Philosopher 5 is Waiting
+Philosopher 1 is Thinking
 
+Dining :3
+Philosopher 3 is Eating
+Philosopher 4 is Waiting
+Philosopher 5 is Waiting
+Philosopher 1 is Thinking
+Philosopher 2 is Thinking
 
+Dining :4
+Philosopher 4 is Eating
+Philosopher 5 is Waiting
+Philosopher 1 is Thinking
+Philosopher 2 is Thinking
+Philosopher 3 is Thinking
+
+Dining :5
+Philosopher 5 is Eating
+Philosopher 1 is Thinking
+Philosopher 2 is Thinking
+Philosopher 3 is Thinking
+Philosopher 4 is Thinking
+
+All philosophers are now Thinking
