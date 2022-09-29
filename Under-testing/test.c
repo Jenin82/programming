f575c14c019ff400078ee5655a9f1167f33da72a
Jenin82 <jenin8282@gmail.com>
2022-09-29 15:20:59 +0530

    new

--- Under-testing/test.c
+++ Under-testing/test.c
@@ -1,40 +1,50 @@
 #include<stdio.h>
-int n,m,i,j,k=-1,p[100],b[100];
+int n,i,j,tq,time=0;
+struct rrb {
+	int id,at,bt,wt,tat,remain;
+}p[20],temp;
 void main() {
-	printf("Enter the no. process and no. memory blocks : ");
-	scanf("%d%d",&n,&m);
-	int flag[m],allocation[m];
-	printf("Enter the size of each process : ");
-	for(i=0;i<n;i++) 
-		scanf("%d",&p[i]);
-	printf("Enter the size of each block : ");
-	for(i=0;i<m;i++){
-		scanf("%d",&b[i]);
-		flag[i]=0;
-		allocation[i]=-1;
-	}
-	for(i=0;i<m;i++) 
-		for(j=0;j<m;j++)
-			if(b[i]<b[j]) {
-				int temp=b[i];
-				b[i]=b[j];
-				b[j]=temp;
-			}
-	for(i=0;i<n;i++) 
-		for(j=0;j<m;j++) 
-			if(flag[j]==0 && b[j]>=p[i]) {
-				allocation[j]=i;
-				flag[j]=1;
-				break;
-			}
-	printf("Process\t\tProcess Size\n");
-	for(i=0;i<n;i++) 
-		printf("%d\t\t\t%d\n",i,p[i]);
-	printf("Block Size\tAllocated Process\n");
-	for(i=0;i<m;i++) {
-		if(allocation[i]!=-1) 
-			printf("%d\t\t\t%d\n",b[i],allocation[i]);
-		else
-			printf("%d\t\t\tNot allocated\n",b[i]);
+	printf("Enter the no. of process :");
+	scanf("%d",&n);
+	printf("Enter the time quantum :");
+	scanf("%d",&tq);
+	for(i=0;i<n;i++) {
+		printf("Enter Arrival time and burst time of P%d :",i);
+		scanf("%d%d",&p[i].at,&p[i].bt);
+		p[i].remain = p[i].bt;
+		p[i].id = i;
 	} 
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			if(p[i].at<p[j].at) {
+				temp = p[i];
+				p[i] = p[j];
+				p[j] = temp;
+			}
+	//for(i=0;i<n;i++)
+		//printf("%d ",p[i].at);
+	i=0;
+	j=0;
+	while(j<n) {
+		if(p[i].remain>0 && p[i].remain<=tq) {
+			time += p[i].remain;
+			p[i].remain = 0;
+			p[i].tat = time-p[i].at;
+			p[i].wt = time-p[i].at-p[i].bt;
+			if(i==n-1)
+				i=-1;
+			i++;
+			printf("%d\n",i);
+		}
+		if(p[i].remain>0) {
+			time += tq;
+			p[i].remain = p[i].remain-tq;
+			printf("%d\n",time);
+			if(i==n-1)
+				i=-1;
+			i++;
+		}
+	}
+	for(i=0;i<n;i++)
+		printf("%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].at,p[i].bt,p[i].wt,p[i].tat);
 }
