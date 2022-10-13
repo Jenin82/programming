7e47275b23e51ec8f6ac814e4aed0d7793b376dc
Jenin82 <jenin8282@gmail.com>
2022-10-13 20:45:22 +0530

    hsjfgsfh

--- Under-testing/test.c
+++ Under-testing/test.c
@@ -1,14 +1,9 @@
 #include<stdio.h>   
-#include<sys/shm.h>  
-int main() 
-{  
-int i;  
-void *shared_memory;  
-char buff[100];  
-int shmid;  
-shmid=shmget((key_t)2345, 1024, 0);  
-printf("Key of shared memory is %d\n",shmid);  
-shared_memory=shmat(shmid,NULL,0); 
-printf("Process attached at %p\n",shared_memory);  
-printf("Data read from shared memory is : %s\n",(char *)shared_memory);  
-}  
+int mutex=1,full=0,empty=3,x;
+int main() {
+	printf("1. Producer/n2. Consumer/n3. Exit/n");
+	scanf("%d",&x);
+	switch(x) {
+		case 1: 
+	}
+}
