a374c02e5930508358f1c5d8ce6948a65c8a40a0
Jenin Joseph <84625875+Jenin82@users.noreply.github.com>
2022-12-09 14:26:35 +0530

    Delete B-Tech-S5/DBMS/PL-SQL directory

--- B-Tech-S5/DBMS/PL-SQL/PL-SQL/exception.sql
+++
@@ -1,7 +0,0 @@
-create table empj(name varchar(30), id int primary key,designation varchar(30), salary float(2));
-insert into empj values('A',101,'Manager',30000);
-insert into empj values('B',102,'SSS',60000);
-insert into empj values('C',103,'SS',50000);
-insert into empj values('D',104,'S',40000);
-insert into empj values('E',105,'A+',30000);
-
